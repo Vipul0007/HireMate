8b72b4a0cf77b56a1c8b32623e7f613772973cf0
Vipul <vipulbansal799@gmail.com>
2025-06-12 11:16:51 +0530

    Create Router for the application

--- HireMate/src/App.jsx
+++ HireMate/src/App.jsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ HireMate/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">Hi I am from Home.jsx</h1>
+  );
+};
+
+export default Home;
